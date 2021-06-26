25df46944a560651777ea350e41a420f2c3dba25
github username <github email address>
2021-06-25 22:36:08 -0400

    Setting up create form.

--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import keynoteReducer from './reducers/keynoteReducer'
+import thunk from 'redux-thunk'
+
+const store = createStore(keynoteReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );

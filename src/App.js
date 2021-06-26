200bcdc25129d222522074d3f4824181ee78bd84
github username <github email address>
2021-06-25 21:43:36 -0400

    Added Bootstrapp css and navbar component.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Navbar from './components/Navbar'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <Router>
+        <div className="App">
+          <Switch>
+            <Navbar />
+          </Switch>
+        </div>
+      </Router>
     )
 }
 }
